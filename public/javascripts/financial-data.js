a7a35617c081d5b2c5d4cfbe79ddbb536224d915
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-05-22 14:39:29 +0200

    iteration 3 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,38 +1,58 @@
+/** @format */
 
+const $inputStart = document.getElementById("startDate");
+const $inputEnd = document.getElementById("endDate");
 
-    axios
-      .get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
-      .then(response => {
-        console.log('Response from API is: ', response.data);
-        printTheChart(response); 
-      })
-      .catch(err => console.log(err));
+axios
+  .get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
 
-      function printTheChart(stockData) {
-        const dailyData = stockData.data.bpi;
-        const date = Object.keys(dailyData);
-        const data = Object.values(dailyData);
+  .then((response) => {
+    console.log("Response from API is: ", response.data);
+    const Datesdetail = response.date;
+    console.log("date detail : ", Datesdetail);
+    printTheChart(response);
+  })
+  .catch((err) => console.log(err));
 
-        const ctx = document.getElementById('my-chart').getContext('2d');
-        const chart = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: date,
-            datasets: [
-              {
-                label: 'Stock Chart',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: data
-              }
-            ]
-          }
-        }); // closes chart = new Chart()
-      } // closes printTheChart()
-  
+// Qd le user click sur le bouton (une fois les dates saisies)
+document.getElementById("date").addEventListener("click", () => {
+  //
+  const value1 = $inputStart.value;
 
-//  document.getElementById('get-country-btn').addEventListener('click', () => {
-    //const userInput = document.getElementById('country-name-input').value;
-   // getCountryInfo(userInput);
-  //});
-  
+  const value2 = $inputEnd.value;
+  console.log(value1, value2);
+  axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${value1}&end=${value2}`
+    )
+
+    .then((response) => {
+      console.log("Response from API is: ", response.data);
+      const Datesdetail = response.date;
+      console.log("date detail : ", Datesdetail);
+      printTheChart(response);
+    })
+    .catch((err) => console.log(err));
+});
+
+function printTheChart(stockData) {
+  const dailyData = stockData.data.bpi;
+  const date = Object.keys(dailyData);
+  const data = Object.values(dailyData);
+
+  const ctx = document.getElementById("my-chart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: date,
+      datasets: [
+        {
+          label: "Stock Chart",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: data,
+        },
+      ],
+    },
+  }); // closes chart = new Chart()
+} // closes printTheChart()
